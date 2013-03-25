c460965c529089e520f6e2ad2a8a011afcb9d338
bingo1st <youbing.song@gmail.com>
2013-03-25 17:15:27 +0800

    Initial Commit

---
+++ Cupid/CPDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CPDAppDelegate.h
+//  Cupid
+//
+//  Created by Apollo on 13-3-25.
+//  Copyright (c) 2013年 Cupid angel team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Cupid/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cupid
+//
+//  Created by Apollo on 13-3-25.
+//  Copyright (c) 2013年 Cupid angel team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CPDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CPDAppDelegate class]));
+    }
+}
