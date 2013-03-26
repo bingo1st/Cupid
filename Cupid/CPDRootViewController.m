c6312e0e0a851b4f9873210d0f471f9158a0f2d5
bingo1st <youbing.song@gmail.com>
2013-03-26 15:49:43 +0800

    Fixed conflicts of merging last time
    Now it can run with icon and flash, Wow wow! and you have to add
    JSONKit supporting before you build it successfully, cause the folder
    is empty.

--- Cupid/CPDRootViewController.m
+++ Cupid/CPDRootViewController.m
@@ -2,17 +2,12 @@
 //  CPDRootViewController.m
 //  Cupid
 //
-<<<<<<< HEAD
-//  Created by Alex on 13-3-25.
-=======
 //  Created by Apollo on 13-3-25.
->>>>>>> db7474f5865be265c339aaa6a47fc21942a169cd
 //  Copyright (c) 2013年 Cupid angel team. All rights reserved.
 //
 
-#import "CPDRootViewController.h"
-<<<<<<< HEAD
 #import "CPDHomeViewController.h"
+#import "CPDRootViewController.h"
 
 static CPDRootViewController *gRootViewController = nil;
 
@@ -33,26 +28,11 @@ static CPDRootViewController *gRootViewController = nil;
     self = [super init];
     if (self) {
 		[[UINavigationBar appearance] setTintColor:[UIColor colorWithHex:0xce4035]];
-=======
-
-@interface CPDRootViewController ()
-
-@end
-
-@implementation CPDRootViewController
-@synthesize controllers;
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
->>>>>>> db7474f5865be265c339aaa6a47fc21942a169cd
     }
     return self;
 }
 
-<<<<<<< HEAD
+
 - (void)dealloc
 {
     [super dealloc];
@@ -69,20 +49,9 @@ static CPDRootViewController *gRootViewController = nil;
 	
 }
 
-
-=======
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    self.title = @"Cupid";
-	// Do any additional setup after loading the view.
-}
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
->>>>>>> db7474f5865be265c339aaa6a47fc21942a169cd
 @end
